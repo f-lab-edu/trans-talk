5c01b88aef4c643868dcd794fb5674627e09d022
khoon9 <kshun08@dongguk.edu>
2025-03-12 17:44:59 +0900

    chore: initialize project

---
+++ src/test/java/flab/transtalk/TranstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.transtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

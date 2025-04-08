0ae2aa960820c2e863897f0ca46eef6ea4448d68
khoon9 <kshun08@dongguk.edu>
2025-04-09 00:49:04 +0900

    feat: 사용자 API 추가
    - 본인 제외 무작위 사용자 호출 get API 추가
    - 특정 사용자 호출 get API 추가

--- src/main/java/flab/transtalk/user/controller/UserController.java
+++ src/main/java/flab/transtalk/user/controller/UserController.java
@@ -2,7 +2,7 @@ package flab.transtalk.user.controller;
 
 import flab.transtalk.user.dto.req.UserCreateRequestDto;
 import flab.transtalk.user.dto.res.UserResponseDto;
-import flab.transtalk.user.service.user.UserSignUpService;
+import flab.transtalk.user.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +14,21 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserSignUpService userSignUpService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserCreateRequestDto dto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(userSignUpService.signUp(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.signUp(dto));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUser(userId));
+    }
+
+    // 주어진 사용자 제외, 무작위 사용자 호출
+    @GetMapping("/matching/{currentUserId}")
+    public ResponseEntity<UserResponseDto> getMatchingUser(@PathVariable Long currentUserId){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserExcept(currentUserId));
     }
 }

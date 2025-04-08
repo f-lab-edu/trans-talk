e7a02617a9b268217c74dee8802ffae4cd6b5120
khoon9 <kshun08@dongguk.edu>
2025-04-09 00:43:30 +0900

    chore: user entity 변경사항 반영
    - 사용자 가입 profile 설정 부분 추가

--- src/main/java/flab/transtalk/user/service/user/UserSignUpService.java
+++ src/main/java/flab/transtalk/user/service/user/UserSignUpService.java
@@ -1,5 +1,7 @@
 package flab.transtalk.user.service.user;
 
+import flab.transtalk.common.enums.LanguageSelection;
+import flab.transtalk.user.domain.Profile;
 import flab.transtalk.user.domain.User;
 import flab.transtalk.user.dto.req.UserCreateRequestDto;
 import flab.transtalk.user.dto.res.UserResponseDto;
@@ -13,11 +15,18 @@ public class UserSignUpService {
 
     private final UserRepository userRepository;
 
-    public UserResponseDto signUp(UserCreateRequestDto dto){
-        User user = userRepository.save(
-                dto.toEntity()
-        );
-        UserResponseDto resDto = UserResponseDto.from(user);
+    public UserResponseDto signUp(UserCreateRequestDto reqDto){
+        User user = reqDto.toEntity();
+
+        Profile profile = Profile.builder()
+                .language(LanguageSelection.KOR)
+                .selfIntroduction("")
+                .build();
+
+        user.setProfile(profile);
+        User savedUser = userRepository.save(user);
+
+        UserResponseDto resDto = UserResponseDto.from(savedUser);
         return resDto;
     }
 }

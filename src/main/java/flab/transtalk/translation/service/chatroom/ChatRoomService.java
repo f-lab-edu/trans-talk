7e35f7e8f966ff0fead80c70d519556a4081aceb
khoon9 <kshun08@dongguk.edu>
2025-04-13 22:36:11 +0900

    refactor: 서비스 레이어 재구성
    - 단순 위임 형태의 서비스 코드 구조를, 해당 메소드를 직접 사용하는 구조로 개선함

--- src/main/java/flab/transtalk/translation/service/chatroom/ChatRoomService.java
+++ src/main/java/flab/transtalk/translation/service/chatroom/ChatRoomService.java
@@ -1,18 +1,52 @@
 package flab.transtalk.translation.service.chatroom;
 
+import flab.transtalk.common.exception.BadRequestException;
+import flab.transtalk.common.exception.NotFoundException;
+import flab.transtalk.common.exception.message.ExceptionMessages;
+import flab.transtalk.translation.domain.ChatRoom;
+import flab.transtalk.translation.domain.ChatRoomUser;
 import flab.transtalk.translation.dto.req.ChatRoomCreateRequestDto;
 import flab.transtalk.translation.dto.res.ChatRoomResponseDto;
+import flab.transtalk.translation.repository.ChatRoomRepository;
+import flab.transtalk.user.domain.User;
+import flab.transtalk.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
 
-    private final ChatRoomCreateService chatRoomCreateService;
+    private final UserRepository userRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
+    @Transactional
     public ChatRoomResponseDto createChatRoom(ChatRoomCreateRequestDto reqDto){
-        return chatRoomCreateService.createChatRoom(reqDto);
+        if (reqDto.getParticipantUserIds().size()<2){
+            throw new BadRequestException(ExceptionMessages.CHAT_ROOM_REQUIRES_AT_LEAST_TWO_USERS);
+        }
+        List<User> users = userRepository.findAllByIdIn(reqDto.getParticipantUserIds());
+        if (users.size() != reqDto.getParticipantUserIds().size()){
+            throw new NotFoundException(ExceptionMessages.USER_NOT_FOUND_IN_PARTICIPANTS);
+        }
+        ChatRoom chatRoom = ChatRoom.builder()
+                .title(reqDto.getTitle())
+                .build();
+
+        List<ChatRoomUser> links = users.stream()
+                .map(user->ChatRoomUser.builder()
+                        .user(user)
+                        .chatRoom(chatRoom)
+                        .build()
+                ).collect(Collectors.toList());
+        chatRoom.getChatRoomUsers().addAll(links);
+
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+        return ChatRoomResponseDto.from(savedChatRoom);
     }
 
 }

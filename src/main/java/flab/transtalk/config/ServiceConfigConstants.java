9d0feaa389ce33fee95d48ccfc30992eb41c9ae4
khoon9 <kshun08@dongguk.edu>
2025-05-25 22:21:06 +0900

    chore: 서비스 설정 클래스 수정
    - 상수만 가지는 유틸리티 클래스이므로 인스터스화 안 되도록 설정

--- src/main/java/flab/transtalk/config/ServiceConfigConstants.java
+++ src/main/java/flab/transtalk/config/ServiceConfigConstants.java
@@ -1,5 +1,9 @@
 package flab.transtalk.config;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ServiceConfigConstants {
     public static final String S3_POST_IMAGE_FOLDER_NAME = "posts/";
     public static final long PRESIGNED_URL_DURATION_HOUR = 6;

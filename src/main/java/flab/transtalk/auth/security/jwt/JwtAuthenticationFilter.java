8d02a4767e7ada2d6a437e99c2c0932c52d35f70
khoon9 <kshun08@dongguk.edu>
2025-05-30 23:21:04 +0900

    refactor: HttpHeaders constant 사용
    - auth header를 읽는 과정에서 기존 하드코딩 방식이 아닌 HttpHeaders constant을 사용하여 불러오도록 수정

--- src/main/java/flab/transtalk/auth/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/flab/transtalk/auth/security/jwt/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,7 +28,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String header = request.getHeader("Authorization");
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
             try {

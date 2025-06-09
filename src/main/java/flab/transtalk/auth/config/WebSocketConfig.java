f32920b1fbbf8051cda388f4e5b80117a778ecff
khoon9 <kshun08@dongguk.edu>
2025-06-09 23:51:44 +0900

    feat: 채팅방 구독 요청에 대한 검증 추가
    - WebSocket을 통해 특정 채팅방에 구독하려는 사용자에 대해 인가 검증을 추가합니다.
    - SUBSCRIBE 메시지가 preSend될 때(구독 등록 전) 사용자가 해당 채팅방의 참여자인지를 확인합니다.

--- src/main/java/flab/transtalk/auth/config/WebSocketConfig.java
+++ src/main/java/flab/transtalk/auth/config/WebSocketConfig.java
@@ -1,12 +1,16 @@
 package flab.transtalk.auth.config;
 
 import flab.transtalk.auth.security.jwt.JwtHandshakeInterceptor;
+import flab.transtalk.common.exception.NotFoundException;
+import flab.transtalk.translation.service.chatroom.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
@@ -15,12 +19,15 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.security.Principal;
+
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+    private final ChatRoomService chatRoomService;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -42,12 +49,32 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             @Override
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
                 StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-                if (accessor != null && accessor.getUser() == null) {
-                    SecurityContext context = (SecurityContext) accessor.getSessionAttributes().get("SPRING_SECURITY_CONTEXT");
-                    if (context != null) {
-                        accessor.setUser(context.getAuthentication());
+                if (accessor != null){
+                    if (accessor.getUser() == null) {
+                        SecurityContext context = (SecurityContext) accessor.getSessionAttributes().get("SPRING_SECURITY_CONTEXT");
+                        if (context != null) {
+                            accessor.setUser(context.getAuthentication());
+                        }
+                    }
+                    if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+                        String destination = accessor.getDestination();
+                        Principal user = accessor.getUser();
+
+                        if (destination == null || !destination.startsWith("/topic/chat-room.") || destination.length()=="/topic/chat-room.".length()) {
+                            throw new MessagingException("잘못된 구독 경로입니다.");
+                        }
+                        Long chatRoomId = Long.valueOf(destination.substring("/topic/chat-room.".length()));
+                        Long userId = Long.valueOf(user.getName());
+                        try {
+                            if (!chatRoomService.isParticipant(userId, chatRoomId)) {
+                                throw new MessagingException("구독 권한이 없습니다.");
+                            }
+                        } catch (NotFoundException ex){
+                            throw new MessagingException("존재하지 않는 채팅방입니다.");
+                        }
                     }
                 }
+
                 return message;
             }
         });

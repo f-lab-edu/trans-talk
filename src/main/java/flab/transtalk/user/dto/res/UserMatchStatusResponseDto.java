17ef08805b5b27aaa499b522df8ef20ca93c4950
khoon9 <kshun08@dongguk.edu>
2025-06-11 21:01:47 +0900

    refactor: 사용자 매칭 상태 DTO에서 lombok Value 어노테이션 제거
    - Value 어노테이션 제거 후 필드에 private 접근제어자 기입

--- src/main/java/flab/transtalk/user/dto/res/UserMatchStatusResponseDto.java
+++ src/main/java/flab/transtalk/user/dto/res/UserMatchStatusResponseDto.java
@@ -1,10 +1,8 @@
 package flab.transtalk.user.dto.res;
 
 import lombok.Builder;
-import lombok.Value;
 
-@Value
 @Builder
 public class UserMatchStatusResponseDto {
-    int remainingMatchRequests;
+    private int remainingMatchRequests;
 }

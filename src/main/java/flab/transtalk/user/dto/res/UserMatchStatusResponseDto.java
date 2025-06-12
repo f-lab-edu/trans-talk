62cb4fdbaec1482099c73e844b6d8c49cf9a4645
khoon9 <kshun08@dongguk.edu>
2025-06-12 18:48:09 +0900

    fix: 사용자 매칭 상태 response DTO 애노테이션 수정
    - 직렬화를 위해 getter 애노테이션이 필요하여 추가함

--- src/main/java/flab/transtalk/user/dto/res/UserMatchStatusResponseDto.java
+++ src/main/java/flab/transtalk/user/dto/res/UserMatchStatusResponseDto.java
@@ -1,7 +1,9 @@
 package flab.transtalk.user.dto.res;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class UserMatchStatusResponseDto {
     private int remainingMatchRequests;
